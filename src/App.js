9bdbafb046967e77ddca83338a2f652b36c03019
Justin Hays <jhays124@yahoo.com>
2018-03-12 15:18:54 -0700

    building skeletons

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Toolbar from './components/Toolbar'
+import Data from './seeds/Data'
 
 class App extends Component {
   render() {
+    console.log(Data)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container">
+        <Toolbar />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
